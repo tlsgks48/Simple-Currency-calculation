84522daffb625f9d83f95114126139d35106245c
tlsgks48 <tlsgks486@gmail.com>
2020-05-11 22:23:15 +0900

    Initial commit

---
+++ app/src/main/java/com/simple_currency_calculation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.simple_currency_calculation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
