1781ebb6a744b788299e7349b4d0468eb7a2dc19
tlsgks48 <tlsgks486@gmail.com>
2020-05-12 22:17:54 +0900

    first activity

--- app/src/main/java/com/simple_currency_calculation/MainActivity.kt
+++ app/src/main/java/com/simple_currency_calculation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.simple_currency_calculation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 수취국가 선택세팅.
+        val colors = arrayOf("한국(KRW)","일본(JPY)","필리핀(PHP)")
+        Currency_Picker.minValue = 0
+        Currency_Picker.maxValue = colors.size-1
+        Currency_Picker.displayedValues = colors
+        // 수취국가 변경시
+        Currency_Picker.setOnValueChangedListener { _, _, newVal ->
+            Currency2.text = "수취국가 : ${colors[newVal]}"
+        }
     }
 }
